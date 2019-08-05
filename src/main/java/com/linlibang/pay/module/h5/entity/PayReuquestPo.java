a2a5cf52f7ac0a7ddaeafc40439a16e71cb63c85
fatYch <897141839@qq.com>
2019-08-05 11:19:02 +0800

    微不足道的修改

---
+++ src/main/java/com/linlibang/pay/module/h5/entity/PayReuquestPo.java
@@ -0,0 +1,16 @@
+package com.linlibang.pay.module.h5.entity;
+
+import com.linlibang.pay.module.base.UnionBaseRequest;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+@ApiModel("下单请求体")
+@Data
+public class PayReuquestPo extends UnionBaseRequest {
+
+    //@ApiModelProperty("订单号")
+
+
+
+}
